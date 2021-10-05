63514a539a0f197b976df9ad7b3937be1b8fdfc9
Sol Parra Arrioja <sparra@ip-192-168-1-15.ec2.internal>
2021-10-04 21:19:17 -0300

    Idea+Apellido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import NavBar from "./Components/NavBar";
+import './App.css';
+import React, { useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <section>
-        <NavBar />
+        <NavBar pedidos={count}/>
       </section>
+      <section >
+                
+                <a class="waves-effect waves-light btn" onClick={() => setCount(count + 1)}>Add to cart</a>
+                <a class="waves-effect waves-light btn" onClick={() => setCount(count - 1)}>Delete</a>
+            </section>
     </div>
   );
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,47 +1,28 @@
 import React from 'react';
+import styles from '../Components/NavBar.module.css';
+import logo from '../img/logo.svg';
 
 
-const NavBar = () => {
+const NavBar = (props) => {
 
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <div class="container-fluid">
-
-                <a class="navbar-brand" href="#">Tiendita de Sol</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown"></li>
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><hr class="dropdown-divider"></hr></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+        <div>
+            <div class="navbar-fixed">
+                <nav>
+                    <div class="nav-wrapper">
+                        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+                        <a href="#!" className={styles.brandlogo}> <img className={styles.imgme} src={logo} alt=""></img></a>
+                        <ul id="nav-mobile"  class="left hide-on-med-and-down">
+                            <li><a href="sass.html"><i class="material-icons black-text">search</i></a></li>
+                            <li><a href="sass.html"><i class="material-icons black-text">person</i></a></li>
+                            <li><div className={styles.count} className='black-text'><span>{props.pedidos}</span></div></li>
+                            <li><a href="sass.html"><i class="material-icons black-text"> add_shopping_cart</i></a></li>
                         </ul>
-
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                        <button class="btn btn-success" type="submit">Search</button>
-                    </form>
-                </div>
-
+                    </div>
+                </nav>
             </div>
-        </nav>
+        </div>
     )
 }
+
 export default NavBar;
